a7e8e8382bb0fadb720f398c8f250d8ed05e6e8e
Matthew Bell <logicnotlogical@outlook.com>
2016-10-26 11:49:22 +0100

    Fix calcBatchSize unit conversions

--- openbrews/recipe/RecipeUtils.js
+++ openbrews/recipe/RecipeUtils.js
@@ -12,51 +12,64 @@ angular.module('openbrews.recipeUtils', [])
         return hops.reduce(function (acc, hop) {
             return acc + this.ibuFromHop(hop, recipe.batchSize, gravity) / hops.length;
         }, 0);
-    }
-
-    this.ibuFromHop = function (hop, batchSize, gravity) {
-        return 1.65 * Math.pow(0.000125, gravity - 1) * ((1 - Math.pow(Math.E, -0.04 * hop.time)) / 4.15) *
-        ((hop.type==="Pellet") ? 1.1 : 1) *
-        (((hop.aa / 100) * this.gramsToOz(hop.grams) * 7490) / this.litersToGallons(batchSize));
     };
 
+    // Calculates the IBU value of a hop.
+    // Values derived from http://howtobrew.com/book/section-1/hops/hop-bittering-calculations
+    this.ibuFromHop = function (hop, batchSize, gravity) {
+        return 1.65 * Math.pow(0.000125, gravity - 1) * ((1 - Math.pow(Math.E, -0.04 * hop.time)) / 4.15) *
+        ((hop.type==='Pellet') ? 1.1 : 1) *
+        (((hop.aa / 100) * this.gramToOz(hop.grams) * 7490) / this.literToGallon(batchSize));
+    };
+
+    // Calculates the boil gravity (the gravity lost during boiling..?) of a recipe.
     this.boilGravity = function (recipe, og) {
-        var boilSize = (recipe.boilSizeUnits === 'L')? this.litersToGallons(recipe.boilSize) : recipe.boilSize;
+        var boilSize = (recipe.boilSizeUnits === 'L')? this.literToGallon(recipe.boilSize) : recipe.boilSize;
 
         return ((this.calcBatchSize(recipe) / boilSize) * (og - 1) + 1);
     };
 
-    this.gramsToOz = function (grams) {
+    // Converts grams to ounces.
+    this.gramToOz = function (grams) {
         return grams * 0.0352739619;
     };
 
-    this.litersToGallons = function (liters) {
+    // Converts liters to gallons.
+    this.literToGallon = function (liters) {
         return liters * 0.26417;
     };
 
-    this.kgsToLbs = function (kgs) {
+
+    // Converts kilograms to pounds.
+    this.kgToLb = function (kgs) {
         return kgs * 2.2046;
     };
 
+    // Converts milliliters to fluid ounces.
     this.mlToFlOz = function (ml) {
         return ml * 0.033814;
-    }
+    };
+
+    // Converts ounces to pounds.
+    this.ozToLb = function (oz) {
+        return oz * 0.0625;
+    };
 
     // Calculate the total batch size of the recipe.
     this.calcBatchSize = function (recipe) {
         var sum = 0;
 
         sum += recipe.fermentables.reduce(function (acc, frmtable) {
-            return acc + (frmtable.weightUnits === 'Kg')? this.kgsToLbs(frmtable.weight) : frmtable.weight;
+            return acc + (frmtable.weightUnits === 'Kg')? this.kgToLb(frmtable.weight) : frmtable.weight;
         });
 
-        sum += recipe.hops.reduce(function (acc, hop) {
-            return acc + (hop.weightUnits === 'G')? this.gramsToOz(hop.weight) : hop.weight;
-        });
+        sum += this.ozToLb(recipe.hops.reduce(function (acc, hop) {
+            return acc + (hop.weightUnits === 'G')? this.gramToOz(hop.weight) : hop.weight;
+        }));
 
-        sum += recipe.items.reduce(function (acc, yeast) {
+        sum += this.ozToLb(recipe.items.reduce(function (acc, yeast) {
             switch (yeast.amountUnits) {
-                case 'G': acc += this.gramsToOz(yeast.amount);
+                case 'G': acc += this.gramToOz(yeast.amount);
                 break;
 
                 case 'ml': acc += this.mlToFlOz(yeast.amount);
@@ -67,11 +80,11 @@ angular.module('openbrews.recipeUtils', [])
             }
 
             return acc;
-        });
+        }));
 
-        sum += recipe.others.reduce(function (acc, item) {
+        sum += this.ozToLb(recipe.others.reduce(function (acc, item) {
             switch (item.amountUnits) {
-                case 'G': acc += this.gramsToOz(item.amount);
+                case 'G': acc += this.gramToOz(item.amount);
                 break;
 
                 case 'ml': acc += this.mlToFlOz(item.amount);
@@ -82,7 +95,7 @@ angular.module('openbrews.recipeUtils', [])
             }
 
             return acc;
-        });
+        }));
 
         return sum;
     };
@@ -117,7 +130,7 @@ angular.module('openbrews.recipeUtils', [])
     this.pointsPerGallon = function (fermentables, batchSize, efficiency) {
 
         return fermentables.reduce(function (acc, frmtable) {
-            var weight = (frmtable.weightUnits === 'Kg')? this.kgsToLbs(frmtable.weight) : frmtable.weight;
+            var weight = (frmtable.weightUnits === 'Kg')? this.kgToLb(frmtable.weight) : frmtable.weight;
 
             if (frmtable.method.match('Mash')) { // Efficiency counts only for mashable fermentables
                 acc += ((frmtable.ppg - 1) * 1000 * weight * (efficiency / 100)) / batchSize;
